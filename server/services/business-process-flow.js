48b7706e0e537b101c2909d50af5ea17e8f12a5c
Mariusz Nowak <medyk@medikoo.com>
2016-06-14 12:44:11 +0200

    (M) Register process flow handler for new service

--- server/services/business-process-flow.js
+++ server/services/business-process-flow.js
@@ -1,1 +1,13 @@
 'use strict';
+
+var includes            = require('es5-ext/array/#/contains')
+  , initialize          = require('eregistrations/server/services/business-process-flow')
+  , db                  = require('../../db')
+  , processingStepsMeta = require('../../apps-common/processing-steps/meta');
+
+initialize(
+	db.BusinessProcessDemo,
+	Object.keys(processingStepsMeta).filter(function (stepShortPath) {
+		return includes.call(processingStepsMeta[stepShortPath]._services, 'demo');
+	})
+);

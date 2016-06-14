2d828d15ba88560bd2cff7b908738de6c8aeaecb
Mariusz Nowak <medyk@medikoo.com>
2016-06-14 12:43:24 +0200

    (M) Register database storage for new service

--- server/business-process-storages.js
+++ server/business-process-storages.js
@@ -1,1 +1,5 @@
 'use strict';
+
+var driver = require('mano').dbDriver;
+
+exports.demo = driver.getStorage('businessProcessDemo');
--- server/services/db/storage-names.js
+++ server/services/db/storage-names.js
@@ -1,3 +1,3 @@
 'use strict';
 
-module.exports = ['user'];
+module.exports = ['businessProcessDemo', 'user'];

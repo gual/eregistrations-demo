703a6fbed4957c2e72995cda2cc2cba445d1e0a7
Mariusz Nowak <medyk@medikoo.com>
2016-06-14 15:35:09 +0200

    (M) Revision Official Role

--- model/user/roles.js
+++ model/user/roles.js
@@ -10,7 +10,9 @@ module.exports = require('eregistrations/model/user')(db);
 db.Role.members.add('metaAdmin');
 db.Role.members.add('user');
 db.Role.members.add('usersAdmin');
+db.Role.members.add('officialRevision');
 
 db.Role.meta.get('metaAdmin').label = _("Meta Admin");
 db.Role.meta.get('user').label = _("User");
 db.Role.meta.get('usersAdmin').label = _("Users Admin");
+db.Role.meta.get('officialRevision').label = _("Revision");

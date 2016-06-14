3256210ab5f2284418ffee5e8f1468b3c2cad009
Mariusz Nowak <medyk@medikoo.com>
2016-06-14 15:57:47 +0200

    (M) Configure processing role

--- model/user/roles.js
+++ model/user/roles.js
@@ -11,8 +11,10 @@ db.Role.members.add('metaAdmin');
 db.Role.members.add('user');
 db.Role.members.add('usersAdmin');
 db.Role.members.add('officialRevision');
+db.Role.members.add('officialProcessing');
 
 db.Role.meta.get('metaAdmin').label = _("Meta Admin");
 db.Role.meta.get('user').label = _("User");
 db.Role.meta.get('usersAdmin').label = _("Users Admin");
 db.Role.meta.get('officialRevision').label = _("Revision");
+db.Role.meta.get('officialProcessing').label = _("Processing");

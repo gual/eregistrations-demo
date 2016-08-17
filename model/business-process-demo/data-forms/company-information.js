4105a4dce63c797b4c872dfea74f3f65bd2089d4
kamsi <gruca.kamil@gmail.com>
2016-08-17 17:01:44 +0200

    Divided company information into two sub sections

--- model/business-process-demo/data-forms/company-information.js
+++ model/business-process-demo/data-forms/company-information.js
@@ -3,17 +3,33 @@
 var db                  = require('../../../db')
   , _                   = require('../../../i18n')
   , FormSection         = require('eregistrations/model/form-section')(db)
+  , FormSectionGroup    = require('eregistrations/model/form-section-group')(db)
   , BusinessProcessDemo = require('../fields');
 
 BusinessProcessDemo.prototype.dataForms.map.define('companyInformation', {
 	nested: true,
-	type: FormSection
+	type: FormSectionGroup
 });
 
-BusinessProcessDemo.prototype.dataForms.map.companyInformation.setProperties({
+var companyInformation = BusinessProcessDemo.prototype.dataForms.map.companyInformation;
+
+companyInformation.setProperties({
 	label: _("Company Information"),
-	actionUrl: 'company-information',
-	propertyNames: ['businessName', 'expectedIncome', 'address/country', 'address/city',
+	actionUrl: 'company-information'
+});
+
+companyInformation.sections.defineProperties({
+	details: { type: FormSection, nested: true },
+	address: { type: FormSection, nested: true }
+});
+
+companyInformation.sections.details.setProperties({
+	propertyNames: ['businessName', 'expectedIncome']
+});
+
+companyInformation.sections.address.setProperties({
+	label: _("Address"),
+	propertyNames: ['address/country', 'address/city',
 		'address/street']
 });
 

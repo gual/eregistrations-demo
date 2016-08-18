f82dfcbe2af1a0b84a221ce775e453fbf7357fcf
kamsi <gruca.kamil@gmail.com>
2016-08-18 14:45:04 +0200

    Included social security requirement condition in companyRegistration

--- model/business-process-demo/registrations/company-registration.js
+++ model/business-process-demo/registrations/company-registration.js
@@ -14,8 +14,14 @@ BusinessProcessDemo.prototype.registrations.map.companyRegistration.setPropertie
 		if (!workers) return;
 		return [this.master.costs.map.companyRegistration];
 	},
-	requirements: function () {
-		var requirementsMap = this.master.requirements.map;
-		return [requirementsMap.inventory, requirementsMap.passport];
+	requirements: function (_observe) {
+		var requirementsMap = this.master.requirements.map, result = [], workers;
+		result = [requirementsMap.inventory, requirementsMap.passport];
+		workers = this.master._get ? _observe(this.master._workers) : this.master.workers;
+
+		if (workers >= 5) {
+			result.push(requirementsMap.socialSecurityCertificate);
+		}
+		return result;
 	}
 });

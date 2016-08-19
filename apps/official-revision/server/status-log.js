a269ee6b48eb3b0a9271b272e4cb157d3e8b3e12
kamsi <gruca.kamil@gmail.com>
2016-08-19 11:20:31 +0200

    Added new status log

--- apps/official-revision/server/status-log.js
+++ apps/official-revision/server/status-log.js
@@ -1,5 +1,20 @@
 'use strict';
 
-var db = require('../../../db');
+var db                = require('../../../db')
+  , _                 = require('../../../i18n').bind('Official: Revision: Status Log')
+  , businessProcesses =
+		db.BusinessProcessDemo.instances.filterByKey('isFromEregistrations', true)
+  , progressPath = 'processingSteps/map/revision/revisionProgress';
 
-module.exports = require('eregistrations/status-logs/official-revision')(db.BusinessProcessDemo);
+module.exports = exports =
+	require('eregistrations/status-logs/official-revision')(db.BusinessProcessDemo);
+
+exports.push({
+	id: 'revision-in-progress',
+	preTrigger: businessProcesses.filterByKeyPath(progressPath, 0),
+	trigger: businessProcesses.filterByKeyPath(progressPath, function (progress) {
+		return progress > 0;
+	}),
+	label: _("Review in progress"),
+	text: _("A review of your request has been started")
+});

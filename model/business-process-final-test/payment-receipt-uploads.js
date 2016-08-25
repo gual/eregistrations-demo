191e196e26f57672c769ba11fd7f49e4f0999cd8
kamsi <gruca.kamil@gmail.com>
2016-08-25 11:30:45 +0200

    Payment receipt uploads

--- model/business-process-final-test/payment-receipt-uploads.js
+++ model/business-process-final-test/payment-receipt-uploads.js
@@ -1,10 +1,13 @@
 'use strict';
 
-var BusinessProcessFinalTest = require('./costs');
+var _                        = require('../../i18n')
+  , BusinessProcessFinalTest = require('./costs');
 
 var definePaymentReceiptUploads =
 	require('eregistrations/model/business-process-new/utils/define-payment-receipt-uploads');
 
 module.exports = definePaymentReceiptUploads(BusinessProcessFinalTest, {
-
+	eregistrationsDeveloperCertificate: {
+		label: _("Proof of payment for Eregistrations Development Certificate")
+	}
 });

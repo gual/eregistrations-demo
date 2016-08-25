35555f4b466df37d8fa929699d907d1eccfb906a
kamsi <gruca.kamil@gmail.com>
2016-08-25 09:51:03 +0200

    Add new service to map utils

--- apps-common/business-processes/index.js
+++ apps-common/business-processes/index.js
@@ -1,3 +1,4 @@
 'use strict';
 
-exports.demo = require('./demo');
+exports.demo      = require('./demo');
+exports.finalTest = require('./final-test');
---
+++ apps-common/business-processes/submitted/final-test.js
@@ -0,0 +1,3 @@
+'use strict';
+
+module.exports = require('../final-test').filterByKey('isSubmitted', true);

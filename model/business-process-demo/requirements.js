fd4c91d9217e82e2860f4481e4a2ce88784bf73c
kamsi <gruca.kamil@gmail.com>
2016-09-09 15:07:34 +0200

    Added legend to requirement

--- model/business-process-demo/requirements.js
+++ model/business-process-demo/requirements.js
@@ -2,11 +2,15 @@
 
 var defineRequirements =
 	require('eregistrations/model/business-process-new/utils/define-requirements');
-var BusinessProcessDemo = require('./base');
+var BusinessProcessDemo = require('./base')
+  , _                   = require('../../i18n').bind("Requirements");
 
 module.exports = defineRequirements(BusinessProcessDemo, [
 	require('../documents/business-plan'),
 	require('../documents/company-registration'),
 	require('../documents/inventory'),
-	require('../documents/passport')
+	{
+		legend: _("Both obverse and reverse"),
+		class: require('../documents/passport')
+	}
 ]);

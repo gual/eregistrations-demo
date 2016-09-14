4974cd85bd7b9f9eef0620fc11876c9d83090902
Marek Tuchowski <marek@tuchowski.com.pl>
2016-09-14 07:55:40 +0200

    Issue #3. Adapt to eregistrations#1574

--- apps/official-front-desk/routes.js
+++ apps/official-front-desk/routes.js
@@ -10,7 +10,7 @@
 var assign = require('es5-ext/object/assign');
 
 require('../../view/print-base');
-require('../../view/user-base');
+require('../../view/base');
 
 assign(exports, require('eregistrations/routes/official')('frontDesk'));
 

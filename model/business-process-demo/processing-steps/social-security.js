ff9ab1a62ba8e30c6f603c8ed59b9dc0450287a6
kamsi <gruca.kamil@gmail.com>
2016-09-15 11:50:01 +0200

    Configure processing step

---
+++ model/business-process-demo/processing-steps/social-security.js
@@ -0,0 +1,46 @@
+'use strict';
+
+var db                  = require('../../../db')
+  , _                   = require('../../../i18n')
+  , ProcessingStep      = require('eregistrations/model/processing-step')(db)
+  , BusinessProcessDemo = require('./base')
+  , FormSection         = require('eregistrations/model/form-section')(db)
+  , DateType            = require('dbjs-ext/date-time/date')(db)
+  , socialSecurityStep;
+
+BusinessProcessDemo.prototype.processingSteps.map.defineProperties({
+	socialSecurity: { type: ProcessingStep, nested: true }
+});
+
+socialSecurityStep = BusinessProcessDemo.prototype.processingSteps.map.socialSecurity;
+
+socialSecurityStep.defineProperties({
+	isSealConfirmed: {
+		type: db.Boolean,
+		label: _("Are the seals of the certificates confirmed?"),
+		required: true
+	},
+	sealDate: {
+		type: DateType,
+		label: _("What is the date of sealing the last certificate?"),
+		required: true
+	}
+});
+
+socialSecurityStep.setProperties({
+	label: _("Social Security"),
+	previousSteps: function () { return [this.owner.revision]; }
+});
+
+socialSecurityStep.getOwnDescriptor('dataForm').type = FormSection;
+
+socialSecurityStep.dataForm.setProperties({
+	actionUrl: function () {
+		return this.master.__id__ + '/social-security-form';
+	},
+	label: _("Seals confirmation"),
+	propertyMasterType: socialSecurityStep.constructor,
+	propertyNames: ['isSealConfirmed', 'sealDate']
+});
+
+module.exports = BusinessProcessDemo;

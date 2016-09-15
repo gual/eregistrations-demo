e990ee4e1906e84c66aa70e4cf557db0dd7d0b62
kamsi <gruca.kamil@gmail.com>
2016-09-15 14:47:13 +0200

    #6 removed useless require

--- view/user-base.js
+++ view/user-base.js
@@ -1,6 +1,5 @@
 'use strict';
 
 require('./base');
-require('eregistrations/view/components/business-process-sent-back-info');
 
 module.exports = exports = require('eregistrations/view/user-base');

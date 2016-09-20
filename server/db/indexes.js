76e918cb018b47cb9b2cf07b1b1b74ce50a8607d
kamsi <gruca.kamil@gmail.com>
2016-09-20 11:31:51 +0200

    #1202 fixed typo

--- server/db/indexes.js
+++ server/db/indexes.js
@@ -42,7 +42,7 @@ userStorage.indexKeyPath('managerDataForms/progress',
 	managers.filterByKeyPath('managerDataForms/progress', 1)).done();
 userStorage.indexKeyPath('isManagerActive',
 	managers.filterByKey('isManagerActive', true)).done();
-userStorage.indexKeyPath('isManagerDesctructionBlocker', managerClients).done();
+userStorage.indexKeyPath('isManagerDestructionBlocker', managerClients).done();
 userStorage.indexKeyPath('canManagedUserBeDestroyed', managerClients).done();
 userStorage.indexKeyPath('canManagerBeDestroyed', managers).done();
 userStorage.indexKeyPath('canBeDestroyed', userAccounts).done();

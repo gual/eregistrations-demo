3550d065840b04b5aab36a3aaac6ed4e9c385380
kamsi <gruca.kamil@gmail.com>
2016-09-26 14:33:00 +0200

    Added legend to requirement upload of company registration

--- model/business-process-demo/requirement-uploads.js
+++ model/business-process-demo/requirement-uploads.js
@@ -2,11 +2,15 @@
 
 var BusinessProcessDemo = require('./base');
 var defineRequirementUploads =
-	require('eregistrations/model/business-process-new/utils/define-requirement-uploads');
+	require('eregistrations/model/business-process-new/utils/define-requirement-uploads')
+  , _                        = require("../../i18n").bind("Requirement uploads");
 
 module.exports = defineRequirementUploads(BusinessProcessDemo, [
 	require('../documents/business-plan'),
-	require('../documents/company-registration'),
+	{
+		class: require('../documents/company-registration'),
+		legend: _("Must be up to date with the national registry.")
+	},
 	require('../documents/inventory'),
 	require('../documents/passport')
 ]);

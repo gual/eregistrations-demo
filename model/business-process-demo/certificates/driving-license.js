c2f5d25cf8f72dd994ec7b663aeb3c8443491427
kamsi <gruca.kamil@gmail.com>
2016-10-10 16:34:44 +0200

    Added simplified driving license certificate definition

--- model/business-process-demo/certificates/driving-license.js
+++ model/business-process-demo/certificates/driving-license.js
@@ -1,21 +1,9 @@
 'use strict';
 
-var db             = require('../../../db')
-  , FormSection    = require('eregistrations/model/form-section')(db)
-  , DrivingLicense = require('../../documents/driving-license');
+var DrivingLicense = require('../../documents/driving-license');
 
 module.exports = DrivingLicense;
 
 DrivingLicense.prototype.defineProperties({
-	dataForm: { type: FormSection },
-	processingStep: { value: function () { return this.master.processingSteps.map.processing; } },
 	isToBeHanded: { value: true }
-});
-
-DrivingLicense.prototype.dataForm.setProperties({
-	label: function () { return this.propertyMaster.label; },
-	disablePartialSubmit: true,
-	actionUrl: function () { return this.master.__id__ + '/certificate/driving-license'; },
-	propertyMasterType: DrivingLicense,
-	propertyNames: ['number', 'files/map', 'issueDate']
 });

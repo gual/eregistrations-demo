5204856d18d2ff7cfecab18a5736f06d86d639e6
kamsi <gruca.kamil@gmail.com>
2016-10-10 16:30:36 +0200

    Cleanup

--- model/business-process-demo/certificates/company-registration.js
+++ model/business-process-demo/certificates/company-registration.js
@@ -1,9 +1,21 @@
 'use strict';
 
-var CompanyRegistration = require('../../documents/company-registration');
+var db                  = require('../../../db')
+  , FormSection         = require('eregistrations/model/form-section')(db)
+  , CompanyRegistration = require('../../documents/company-registration');
 
 module.exports = CompanyRegistration;
 
 CompanyRegistration.prototype.defineProperties({
+	dataForm: { type: FormSection },
+	processingStep: { value: function () { return this.master.processingSteps.map.processing; } },
 	isToBeHanded: { value: true }
+});
+
+CompanyRegistration.prototype.dataForm.setProperties({
+	label: function () { return this.propertyMaster.label; },
+	disablePartialSubmit: true,
+	actionUrl: function () { return this.master.__id__ + '/certificate/company-registration'; },
+	propertyMasterType: CompanyRegistration,
+	propertyNames: ['number', 'files/map', 'issueDate']
 });

cfd9ea2d59513478a8cd7b9fdd9aef3580b16672
kamsi <gruca.kamil@gmail.com>
2016-10-10 16:14:48 +0200

    Simplified certificate definition

--- model/business-process-demo/certificates/company-registration.js
+++ model/business-process-demo/certificates/company-registration.js
@@ -1,21 +1,9 @@
 'use strict';
 
-var db                  = require('../../../db')
-  , FormSection         = require('eregistrations/model/form-section')(db)
-  , CompanyRegistration = require('../../documents/company-registration');
+var CompanyRegistration = require('../../documents/company-registration');
 
 module.exports = CompanyRegistration;
 
 CompanyRegistration.prototype.defineProperties({
-	dataForm: { type: FormSection },
-	processingStep: { value: function () { return this.master.processingSteps.map.processing; } },
 	isToBeHanded: { value: true }
-});
-
-CompanyRegistration.prototype.dataForm.setProperties({
-	label: function () { return this.propertyMaster.label; },
-	disablePartialSubmit: true,
-	actionUrl: function () { return this.master.__id__ + '/certificate/company-registration'; },
-	propertyMasterType: CompanyRegistration,
-	propertyNames: ['number', 'files/map', 'issueDate']
 });

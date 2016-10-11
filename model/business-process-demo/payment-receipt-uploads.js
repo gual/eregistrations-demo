d16e9f124a751438937ef62ff48308d26aff27f1
Marek Tuchowski <marek@tuchowski.com.pl>
2016-10-11 12:10:52 +0200

    Remove unused i18n require

--- model/business-process-demo/payment-receipt-uploads.js
+++ model/business-process-demo/payment-receipt-uploads.js
@@ -1,7 +1,6 @@
 'use strict';
 
-var _                   = require('../../i18n')
-  , BusinessProcessDemo = require('./costs');
+var BusinessProcessDemo = require('./costs');
 
 var definePaymentReceiptUploads =
 	require('eregistrations/model/business-process-new/utils/define-payment-receipt-uploads');

8ea3ab1a3a94fc6a815d59b2ce8583fc4e198587
Marek Tuchowski <marek@tuchowski.com.pl>
2016-10-11 10:40:29 +0200

    Add legend to companyRegistration payment receipt upload

--- model/business-process-demo/payment-receipt-uploads.js
+++ model/business-process-demo/payment-receipt-uploads.js
@@ -7,5 +7,8 @@ var definePaymentReceiptUploads =
 	require('eregistrations/model/business-process-new/utils/define-payment-receipt-uploads');
 
 module.exports = definePaymentReceiptUploads(BusinessProcessDemo, {
-	companyRegistration: { label: _("Proof of payment for company registration") }
+	companyRegistration: {
+		label: _("Proof of payment for company registration"),
+		legend: _("You need to pay it at the nearest bank to receive proper proof of payment")
+	}
 });

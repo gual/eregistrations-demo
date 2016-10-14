0f82fc0cb080917618dba97f486b1fdd397c540d
Marek Tuchowski <marek@tuchowski.com.pl>
2016-10-14 10:34:55 +0200

    Make companyRegistration payment receipt upload to handle both companyRegistration as well as certificateOfIncentives cost

--- model/business-process-demo/payment-receipt-uploads.js
+++ model/business-process-demo/payment-receipt-uploads.js
@@ -7,5 +7,11 @@ var definePaymentReceiptUploads =
 	require('eregistrations/model/business-process-new/utils/define-payment-receipt-uploads');
 
 module.exports = definePaymentReceiptUploads(BusinessProcessDemo, {
-	companyRegistration: { label: _("Proof of payment for company registration") }
+	companyRegistration: {
+		costs: [
+			BusinessProcessDemo.prototype.costs.map.companyRegistration,
+			BusinessProcessDemo.prototype.costs.map.certificateOfIncentives
+		],
+		label: _("Proof of payment for company registration")
+	}
 });

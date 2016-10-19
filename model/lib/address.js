3f195dcc0651f8e557e2468946455b7262657db0
kamsi <gruca.kamil@gmail.com>
2016-10-19 16:56:41 +0200

    Added postalCode formatting

--- model/lib/address.js
+++ model/lib/address.js
@@ -9,5 +9,8 @@ module.exports = db.Object.extend('Address', {
 	country: { type: Country, label: _("Country"), required: true },
 	city: { type: StringLine, label: _("City"), required: true },
 	street: { type: StringLine, label: _("Street or location"), required: true },
-	postalCode: { type: StringLine, label: _("Postal code"), required: true }
+	postalCode: { type: StringLine, label: _("Postal code"),
+		pattern: /^\d\d-\d\d\d$/,
+		inputMask: '88-888',
+		required: true }
 });

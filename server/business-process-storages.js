c44bb93e58964892f0c4e1c6a8ebbb57d1a20e9e
Marek Tuchowski <marek@tuchowski.com.pl>
2016-11-08 14:50:24 +0100

    Register the storage for the new service

--- server/business-process-storages.js
+++ server/business-process-storages.js
@@ -3,3 +3,4 @@
 var driver = require('mano').dbDriver;
 
 exports.demo = driver.getStorage('businessProcessDemo');
+exports.otherOne = driver.getStorage('businessProcessOtherOne');
--- server/services/db/storage-names.js
+++ server/services/db/storage-names.js
@@ -1,3 +1,3 @@
 'use strict';
 
-module.exports = ['businessProcessDemo', 'user', 'object'];
+module.exports = ['businessProcessDemo', 'businessProcessOtherOne', 'user', 'object'];
